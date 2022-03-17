e5348ffcbba3fe7da4cec2bbfa4001edc6a6be1f
Király Tamás <tomaszpest@gmail.com>
2022-03-17 21:00:25 +0100

    Teljes Siker

--- ProductTemplates/productsapp/views.py
+++ ProductTemplates/productsapp/views.py
@@ -2,24 +2,36 @@ from django.shortcuts import render
 
 def electronics(request):
     product_dict={
+        'heading':'ELECTRONICS',
+        'image1':'image/Mac.jpg',
         'product1':'Mac',
-        'product2':'IPhone',
+        'image2':'image/Android.jpg',
+        'product2':'Android',
+        'image3':'image/Dell.jpg',
         'product3':'Dell'
     }
     return render(request, 'productsapp/products.html', product_dict)
 
 def toys(request):
     product_dict={
+        'heading':'TOYS',
+        'image1':'image/TeddyBear.jpg',
         'product1':'Teddy Bear',
+        'image2':'image/Doll.jpg',
         'product2':'Doll',
+        'image3':'image/Wow.jpg',
         'product3':'World of Warcraft'
     }
     return render(request, 'productsapp/products.html', product_dict)
 
 def shoes(request):
     product_dict={
+        'heading':'SHOES',
+        'image1':'image/Adidas.jpg',
         'product1':'Adidas',
+        'image2':'image/Nike.jpg',
         'product2':'Nike',
+        'image3':'image/Tisza.jpg',
         'product3':'Tisza'
     }
     return render(request, 'productsapp/products.html', product_dict)
