35877dd8851e42fa386de29d4e5252cf995088d9
Király Tamás <tomaszpest@gmail.com>
2022-03-19 22:46:57 +0100

    Alakítás

--- ModelDemo/empapp/admin.py
+++ ModelDemo/empapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from empapp.models import Employee
 
+'''Ezzel a Class-el lehet beállítani, hogy az Admin felületen, mikor az Employee menüben vagyunk,
+milyen lista alapján jelenítse meg az Employee tagokna ka listáját.'''
 class EmployeeAdmin(admin.ModelAdmin):
     list_display=['firstName','lastName','salary','email']
 
--- ModelDemo/empapp/models.py
+++ ModelDemo/empapp/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Employee(models.Model):
-    firstName=models.CharField(max_length=40)
-    lastName=models.CharField(max_length=40)
-    salary=models.FloatField()
-    email=models.EmailField()
+    firstName=models.CharField(max_length=40, verbose_name='Vezetéknév')
+    lastName=models.CharField(max_length=40, verbose_name='Keresztnév')
+    salary=models.FloatField(verbose_name='Fizetés')
+    email=models.EmailField(verbose_name='Email cím')
+    
+    def __str__(self):
+        return self.firstName+str(" ")+self.lastName
