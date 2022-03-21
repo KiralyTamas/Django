4e613ac7adf2c8db006ca7129860c30e5c5bce50
Király Tamás <tomaszpest@gmail.com>
2022-03-21 19:44:12 +0100

    Kész vagyok a feladattal.

--- Passenger/Passenger/settings.py
+++ Passenger/Passenger/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'Passenger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'passenger',
+        'USER':'root',
+        'PASSWORD':'King2022'
     }
 }
 
