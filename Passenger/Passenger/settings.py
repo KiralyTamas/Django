cc7e862b32ae9e12bd7b61874b2dae364903d7cc
KiralyTamas <tomaszpest@gmail.com>
2022-03-21 15:29:04 +0100

    Végre siker

--- Passenger/Passenger/settings.py
+++ Passenger/Passenger/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES=[
-    BASE_DIR / STATIC_URL
-]
+STATICFILES_DIRS=[BASE_DIR / STATIC_URL]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
