7c126df5dc437e350a16d4d78b870af9213abe39
KiralyTamas <tomaszpest@gmail.com>
2022-03-22 12:33:59 +0100

    Update

--- ModelDemo/ModelDemo/settings.py
+++ ModelDemo/ModelDemo/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'ModelDemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'employeedb',
-        'USER':'root',
-        'PASSWORD':'King2022'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':BASE_DIR/'db.sqlite3'
+        #'NAME': 'employeedb',
+        #'USER':'root',
+        #'PASSWORD':'King2022'
         
     }
 }
