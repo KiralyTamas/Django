2b28d7a79423ae495ba9b26296d692e1f3ad068d
Király Tamás <tomaszpest@gmail.com>
2022-03-25 21:56:25 +0100

    A hibaüzenetnek utánas kell nézni.

--- FormsDemo/formsapp/forms.py
+++ FormsDemo/formsapp/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
 class UserRegistration(forms.Form):
+    GENDER=[('male','MALE'),('female','FEMALE')]
     firstName=forms.CharField(label='Vezeték Név')
     lastName=forms.CharField(label='Kereszt Név')
     email=forms.EmailField(label='Email Cím')
+    password=forms.CharField(widget=forms.PasswordInput)
+    gender=forms.CharField(widget=forms.Select(choices=GENDER))
+    ssn=forms.IntegerField()
+    
+    def clean_firstName(self):
+        inputfirstName=self.cleaned_data['firstName']
+        if len(inputfirstName)>8:
+            raise forms.ValidationError('Maximális karakterszám: 8')
+        return inputfirstName
--- FormsDemo/formsapp/views.py
+++ FormsDemo/formsapp/views.py
@@ -4,4 +4,12 @@ from . import forms
 def userRegistrationForm(request):
     form=forms.UserRegistration()
     form_dict={'form':form}
+    if request.method=='POST':
+        form=forms.UserRegistration(request.POST)
+        if form.is_valid():
+            print("Form is valid")
+            print('Vezeték Név:', form.cleaned_data['firstName'])
+            print('Kereszt Név:', form.cleaned_data['lastName'])
+            print('Email Cím:', form.cleaned_data['email'])
+            print('Neme:', form.cleaned_data['gender'])
     return render(request,'formsdemo/userregistration.html',form_dict)
