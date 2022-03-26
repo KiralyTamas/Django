a62269022c83b87fba70e5345189ca82836cb0e6
Király Tamás <tomaszpest@gmail.com>
2022-03-26 20:57:52 +0100

    Jól haladtunk.

--- FormsDemo/FormsDemo/settings.py
+++ FormsDemo/FormsDemo/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'FormsDemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'formsdemo',
-        #'USER':'root',
-        #'PASSWORD':'King2022',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'formsdemo',
+        'USER':'root',
+        'PASSWORD':'King2022',
     }
 }
 
--- FormsDemo/formsapp/forms.py
+++ FormsDemo/formsapp/forms.py
@@ -1,19 +1,43 @@
 from django import forms
+from django.core import validators
 
 class UserRegistration(forms.Form):
     GENDER=[('male','MALE'),('female','FEMALE')]
-    firstName=forms.CharField(label='Vezeték Név')
+    firstName=forms.CharField(label='Vezeték Név',validators=
+                        [validators.MinLengthValidator(3),validators.MaxLengthValidator(8)])
     lastName=forms.CharField(label='Kereszt Név')
     email=forms.EmailField(label='Email Cím')
     password=forms.CharField(widget=forms.PasswordInput, label='Jelszó')
     gender=forms.CharField(widget=forms.Select(choices=GENDER),label='Neme')
     ssn=forms.IntegerField()
-    
+"""
+    def clean(self):
+        user_cleaned_data=super().clean()
+        max_len=8
+        pass_len=4
+        inputfirstName=user_cleaned_data['firstName']
+        if len(inputfirstName)>max_len:
+            raise forms.ValidationError(f'Maximális karakterszám: {max_len}')
+        inputEmail=user_cleaned_data['email']
+        if inputEmail.find('@')==-1:
+            raise forms.ValidationError('Nem megfelelő Email formátum')
+        inputPassword=user_cleaned_data['password']
+        if len(inputPassword)<pass_len:
+            raise forms.ValidationError(f'A jelszónak minimum {pass_len} karakter hosszúnak kell lennie!')
+        return
+
     def clean_firstName(self):
+        max_len=8
         inputfirstName=self.cleaned_data['firstName']
-        if len(inputfirstName)>8:
-            raise forms.ValidationError('Maximális karakterszám: 8')
+        if len(inputfirstName)>max_len:
+            raise forms.ValidationError(f'Maximális karakterszám: {max_len}')
         return inputfirstName
+    
+    def clean_email(self):
+        inputEmail=self.cleaned_data['email']
+        if inputEmail.find('@')==-1:
+            raise forms.ValidationError('Nem megfelelő Email formátum')
+        return inputEmail
     
     def clean_password(self):
         pass_len=4
@@ -21,4 +45,5 @@ class UserRegistration(forms.Form):
         if len(inputpassword)<pass_len:
             raise forms.ValidationError(f'A jelszónak minimum {pass_len} karakter hosszúnak kell lennie!')
         return inputpassword
-        
+
+"""
