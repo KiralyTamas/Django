38d5214a15502bdf97559ecb8b705407ab50ccae
KiralyTamas <tomaszpest@gmail.com>
2022-03-29 13:38:18 +0200

    Kész

--- ModelFormsDemo/ModelFormsDemo/settings.py
+++ ModelFormsDemo/ModelFormsDemo/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'projectdb',
+        #'USER':'root',
+        #'PASSWORD':'King2022'
     }
 }
 
