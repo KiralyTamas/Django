6460bdbae52f2b05496d5afe96272d2f8c960a7d
Király Tamás <tomaszpest@gmail.com>
2022-03-31 18:41:13 +0200

    Frissítés

--- CourseCollector/courseApp/models.py
+++ CourseCollector/courseApp/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Course(models.Model):
     courseName=models.CharField(max_length=40,verbose_name='Tananyag Név')
     description=models.TextField(verbose_name='Tananyag Leírás')
+    instructor=models.CharField(default='', max_length=40, verbose_name='Tanár')
     rates=models.FloatField(verbose_name='Értékelés')
