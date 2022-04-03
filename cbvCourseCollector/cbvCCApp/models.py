8d647c51984c65ffc2a649dbf6c54521c8d0d630
Király Tamás <tomaszpest@gmail.com>
2022-04-03 14:02:56 +0200

    Kész vagyok a feladattal :)

--- cbvCourseCollector/cbvCCApp/models.py
+++ cbvCourseCollector/cbvCCApp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 class Course(models.Model):
     courseName=models.CharField(max_length=40,verbose_name='Tananyag Név')
     description=models.TextField(verbose_name='Tananyag Leírás')
     instructor=models.CharField(max_length=40, verbose_name='Tanár')
     rates=models.FloatField(verbose_name='Értékelés')
+    
+    def get_absolute_url(self):
+        return reverse("index")
+    
--- cbvCourseCollector/cbvCCApp/views.py
+++ cbvCourseCollector/cbvCCApp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from cbvCCApp.models import Course
 from django.views.generic import ListView,CreateView,UpdateView,DeleteView,DetailView
+from django.urls import reverse_lazy
 
 class CourseList(ListView):
     model=Course
@@ -9,3 +9,18 @@ class CourseList(ListView):
 class CourseDetail(DetailView):
     model=Course
     template_name='cbvCCApp/detail.html'
+    
+class CourseUpdate(UpdateView):
+    model=Course
+    fields=['courseName','description','instructor','rates']
+    template_name='cbvCCApp/update.html'
+    
+class CourseDelete(DeleteView):
+    model=Course
+    template_name='cbvCCApp/delete.html'
+    success_url=reverse_lazy('index')
+    
+class CourseCreate(CreateView):
+    model=Course
+    fields=['courseName','description','instructor','rates']
+    template_name='cbvCCApp/create.html'
