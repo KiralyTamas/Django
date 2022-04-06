4ff314350e5ace6a6916e5a0a240c7cbcdfa4111
Király Tamás <tomaszpest@gmail.com>
2022-04-06 20:53:46 +0200

    Ez most egy kicsit zavaros volt :)

--- sessionAPI/sessionAPI/urls.py
+++ sessionAPI/sessionAPI/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sessionApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('count/',views.pageCount),
+    path('addItem/',views.addItem),
+    path('',views.index),
+    path('displayItems/',views.displayCart),
 ]
