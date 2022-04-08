39df2704b05b8c23ec684d88581f791d96936546
Király Tamás <tomaszpest@gmail.com>
2022-04-08 12:34:44 +0200

    Erre rá kell majd feküdni, de nagyon hasznos.

--- ModelDemo/empapp/models.py
+++ ModelDemo/empapp/models.py
@@ -8,3 +8,36 @@ class Employee(models.Model):
     
     def __str__(self):
         return self.firstName+str(" ")+self.lastName
+
+class Programmer(models.Model):
+    name=models.CharField(max_length=40, verbose_name='Programozó Neve')
+    salary=models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    name=models.CharField(max_length=40)
+    programmers=models.ManyToManyField(Programmer)
+    
+    def __str__(self):
+        return self.name
+    
+class Customer(models.Model):
+    name=models.CharField(max_length=40)
+    
+class PhoneNumber(models.Model):
+    typee=models.CharField(max_length=10)
+    number=models.CharField(max_length=10)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    
+class Person(models.Model):
+    firstName=models.CharField(max_length=20)
+    lastName=models.CharField(max_length=20)
+    age=models.IntegerField()
+    
+class License(models.Model):
+    typ=models.CharField(max_length=10)
+    validFrom=models.DateField()
+    validTo=models.DateField()
+    person=models.OneToOneField(Person,on_delete=models.CASCADE)
